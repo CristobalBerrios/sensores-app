b9384f412952eb12b6f1622623dc484cbb036ff9
Cristobal Berrios <elcriss96@gmail.com>
2018-02-25 19:29:05 -0300

    se agrega ruta de la app

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,6 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   address: {
-    api: '"http://localhost:3001/api/"'
+    api: '"http://localhost:3001/api/"',
+    spa: '"http://localhost:8080/"'
   }
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   address: {
-    api: '"https://api-sismos.herokuapp.com/api/"'
+    api: '"https://api-sismos.herokuapp.com/api/"',
+    spa: '"https://sismos-app.herokuapp.com/"'
   }
 }
