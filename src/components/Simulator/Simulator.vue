72aca1903b951a816e15ccfddd0fabc0ed4e6527
Cristobal Berrios <elcriss96@gmail.com>
2018-02-25 18:52:45 -0300

    se agrega frames y se cargan los eventos

--- src/components/Simulator/Simulator.vue
+++ src/components/Simulator/Simulator.vue
@@ -1,15 +1,32 @@
 <template>
   <v-card class="card-content elevation-3">
     <v-layout row wrap>
-      <v-flex xs10>
+      <v-flex xs12 md8 lg8>
         <h1 class="display-1">MONITOR DE SISMOS EN TIEMPO REAL</h1>
-        <br>
+      </v-flex>
+      <v-flex xs12 md4 lg4>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <p class="title-scale">Escala Mercalli (Numérica)</p>
+          </v-flex>
+          <v-flex xs12>
+            <img :src="scale" width="100%" alt="">
+          </v-flex>
+        </v-layout>
       </v-flex>
       <v-flex xs12 md4>
-        <monitoreo-card style="margin: 2px;"></monitoreo-card>
+        <v-card class="frame elevation-3">
+          <monitoreo-card 
+          :intensity="last_event.intensity"
+          :date="moment(last_event.date).format('DD-MM-YYYY')"
+          :hour="moment(last_event.date).format('hh:mm A')"
+          style="margin: 2px;">
+          </monitoreo-card>
+        </v-card>
       </v-flex>
       <v-flex xs12 md8>
-        <gmap-map
+        <v-card class="frame elevation-3">
+          <gmap-map
         :center="{lat:-33.4, lng:-71.6}"
         :zoom="9"
         map-type-id="terrain"
@@ -18,7 +35,7 @@
         :draggable="true">
           <div v-for="(sensor, index) in sensores" :key="index">
             <gmap-marker
-            v-if="index == 1"
+            v-if="isEvent(sensor._id)"
           :title="sensor.name"
           :position="sensor.position"
           :icon="{url:iconMarkerRed}"
@@ -35,6 +52,24 @@
           </gmap-marker>
           </div>
         </gmap-map>
+        </v-card>
+      </v-flex>
+      <v-flex xs12>
+        <h2>REGISTRO DE EVENTOS DETECTADOS</h2>
+      </v-flex>
+      <v-flex xs12>
+        <v-data-table
+        :headers="headers"
+        :items="events"
+        hide-actions
+        class="elevation-1">
+        <template slot="items" slot-scope="props">
+          <td class="text-xs-lefts">{{ moment(props.item.date).format('DD-MM-YYYY') }}</td>
+          <td class="text-xs-left">{{  moment(props.item.date).format('hh:mm A') }}</td>
+          <td class="text-xs-left">{{ props.item.intensity }}</td>
+          <td class="text-xs-left">{{ props.item.sensors.length }}</td>
+        </template>
+      </v-data-table>
       </v-flex>
     </v-layout>
   </v-card>
@@ -43,24 +78,36 @@
 <script>
 import MonitoreoCard from '@/components/Simulator/MonitoreoCard'
 import {sensorService} from '@/services/Sensor.service'
+import {eventService} from '@/services/Event.service'
 import iconGreen from '@/assets/marker-green.png'
 import iconRed from '@/assets/marker-red.png'
+import Scale from '@/assets/escala.png'
 export default {
   data () {
     return {
-      sensores: {
-        position: {
-          lat: Number,
-          lng: Number
-        }
+      headers: [
+        {text: 'Fecha', align: 'left', sortable: false},
+        {text: 'Hora', align: 'left', sortable: false},
+        {text: 'intensidad', align: 'left', sortable: false},
+        {text: 'N° Sensores Activados', align: 'left', sortable: false}
+      ],
+      sensores: [],
+      events: [],
+      last_event: {
+        intensity: 0,
+        date: '00-00-0000',
+        hour: '00-00',
+        sensors: []
       },
       iconMarkerGreen: iconGreen,
-      iconMarkerRed: iconRed
+      iconMarkerRed: iconRed,
+      scale: Scale
     }
   },
   components: {MonitoreoCard},
   mounted () {
     let vm = this
+    vm.getEvents()
     vm.getSensores()
   },
   methods: {
@@ -69,8 +116,41 @@ export default {
       sensorService.query().then(data => {
         vm.sensores = data.body
       })
+    },
+    getEvents () {
+      let vm = this
+      eventService.query().then(data => {
+        vm.events = data.body
+        if (vm.events.length > 0) vm.last_event = vm.events[0]
+      })
+    },
+    isEvent (id) {
+      let vm = this
+      let sensors = vm.last_event.sensors
+      let is = false
+      sensors.forEach(data => {
+        if (data === id) {
+          is = true
+        }
+      })
+
+      return is
     }
   }
 }
 </script>
+
+<style scoped>
+  .title-scale {
+    margin: 0;
+  }
+
+  .frame {
+    padding: 10px;
+    border-radius: 10px;
+    background-color: #EEEEEE;
+    margin: 4px;
+  }
+</style>
+
 
