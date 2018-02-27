a9a40e325f8efe2e2f6dec09fa131405cb4b626c
Cristobal Berrios <elcriss96@gmail.com>
2018-02-26 22:34:03 -0300

    se cambia el toolbar

--- src/components/Admin/App.vue
+++ src/components/Admin/App.vue
@@ -1,7 +1,10 @@
 <template>
   <section>
-    <v-toolbar color="green darken-1" dark>
-      <v-toolbar-title>Sysmos SpA</v-toolbar-title>
+    <v-toolbar class="fondo" dark>
+      <img class="logo" src="../../assets/logo-sysmos.gif" alt="">
+      <v-toolbar-title class="toolbar-title">
+        Sysmos SpA
+      </v-toolbar-title>
       <v-toolbar-items class="hidden-xs-and-down">
         <v-btn @click="go('heartBeat')" flat>Heartbeat</v-btn>
         <v-btn @click="go('simulator')" flat>Monitor</v-btn>
@@ -44,6 +47,31 @@ export default {
   padding: 20px;
   margin: 10px;
   border-radius: 15px;
+}
+
+.fondo {
+  background-image: url("../../assets/banner-fondo.jpg") !important;
+  background-repeat: no-repeat !important;
+  background-size: cover !important;
+  background-position: left center !important;
+}
+
+.logo {
+  height: 45px;
+  position: relative;
+}
+.toolbar-title {
+  margin-left: 0px;
+  margin-right: 50px;
+  font-size: 23px;
+}
+
+.table.table thead th {
+  font-size: 17px;
+}
+
+thead {
+  background-color: #e6f7ff;
 }
 </style>
 
