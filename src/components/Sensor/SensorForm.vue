48b278bde0d4b6e5e66669633e7345cfd730b7d0
Cristobal Berrios <elcriss96@gmail.com>
2018-03-02 12:22:12 -0300

    se agrega select cluster

--- src/components/Sensor/SensorForm.vue
+++ src/components/Sensor/SensorForm.vue
@@ -12,20 +12,30 @@
         <v-container grid-lis-md>
           <v-layout wrap>
             <v-flex xs12>
-              <v-text-field v-model="sensor.id" label="ID No" color="green darken-1" required></v-text-field>
+              <v-text-field v-model="sensor.id" label="ID No" color="light-blue darken-2" required></v-text-field>
             </v-flex>
             <v-flex xs12>
-              <v-text-field v-model="sensor.name" label="Nombre" color="green darken-1" required></v-text-field>
+              <v-text-field v-model="sensor.name" label="Nombre" color="light-blue darken-2" required></v-text-field>
             </v-flex>
             <v-flex xs12>
-              <v-text-field type="number" v-model="sensor.position.lat" label="Latitud" color="green darken-1" required></v-text-field>
+              <v-select
+                label="Cluster"
+                :items="clusters"
+                item-text="name"
+                item-value="_id"
+                v-model="sensor.cluster"
+                required
+              ></v-select>
+              </v-flex>
+            <v-flex xs12>
+              <v-text-field type="number" v-model="sensor.position.lat" label="Latitud" color="light-blue darken-2" required></v-text-field>
             </v-flex>
             <v-flex xs12>
-              <v-text-field type="number" v-model="sensor.position.lng" label="Longitud" color="green darken-1" required></v-text-field>
+              <v-text-field type="number" v-model="sensor.position.lng" label="Longitud" color="light-blue darken-2" required></v-text-field>
             </v-flex>
             <v-flex xs12>
               <v-btn :loading="loading" v-if="title === 'Nuevo HeartBeat'" block color="light-blue darken-2" dark @click="submitSensor(sensor)">Agregar</v-btn>
-              <v-btn v-else block color="light-blue darken-2" dark @click="updateSensor(sensor)">Actualizar</v-btn>
+              <v-btn :loading="loading" v-else block color="light-blue darken-2" dark @click="updateSensor(sensor)">Actualizar</v-btn>
             </v-flex>
           </v-layout>
         </v-container>
@@ -37,6 +47,7 @@
 
 <script>
 import {sensorService} from '@/services/Sensor.service'
+import {clusterService} from '@/services/Cluster.service'
 export default {
   data () {
     return {
@@ -46,12 +57,14 @@ export default {
           lng: Number
         }
       },
+      clusters: [],
       title: '',
       loading: false
     }
   },
   created () {
     let vm = this
+    vm.getClusters()
     vm.$parent.$on('crear', () => {
       vm.title = 'Nuevo HeartBeat'
     })
@@ -71,17 +84,30 @@ export default {
       let vm = this
       vm.loading = true
       sensorService.save(model).then(data => {
-        vm.$emit('newSensor', data.body.sensor)
+        vm.$emit('newSensor', data.body)
         vm.closeDialog()
+        vm.loading = false
       })
     },
     updateSensor (sensor) {
       let vm = this
       vm.loading = true
       sensorService.update(sensor._id, sensor).then(data => {
-        console.log(data)
+        vm.loading = false
         vm.$emit('updateSensor')
         vm.$emit('closeDialog')
+        vm.sensor = {
+          position: {
+            lat: Number,
+            lng: Number
+          }
+        }
+      })
+    },
+    getClusters () {
+      let vm = this
+      clusterService.query().then(data => {
+        vm.clusters = data.body
       })
     },
     closeDialog () {
